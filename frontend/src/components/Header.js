8e906913c5f464e0636abcf051b4e13418ff7706
XI4507 <rakeshsingh9625@gmail.com>
2023-09-22 13:13:19 +0530

    add product screen and rating component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import {Navbar,Nav,Container} from 'react-bootstrap'
 import {FaShoppingCart,FaUser} from 'react-icons/fa'
+import {LinkContainer} from 'react-router-bootstrap'
+import logo from '../assets/logo.png'
 
 const Header = () => {
   return (
     <header>
         <Navbar bg='dark' variant='dark' expand='me' collapseOnSelect>
             <Container>
-                <Navbar.Brand href='/'>ProShop</Navbar.Brand>
+            <LinkContainer to='/'>
+            <Navbar.Brand>
+              <img src={logo} alt='ProShop' />
+              ProShop
+            </Navbar.Brand>
+          </LinkContainer>
                 <Navbar.Toggle aria-controls='basic-navbar-nav'/>
                 <Navbar.Collapse id='basic-navbar-nav'>
                     <Nav className='ms-auto'>
